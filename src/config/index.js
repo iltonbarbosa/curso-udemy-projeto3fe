c171aefba0bb511d495418f3afb58b9629922071
Ilton Barbosa <jiltonbarbosa@gmail.com>
2021-03-30 14:33:25 -0300

    30-03-2021

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 export default {
-  url:
-    'https://dashboard.heroku.com/apps/strapi-landing-pages-project/pages/?slug=',
+  url: 'https://strapi-landing-pages-project.herokuapp.com/',
   //url: 'http://localhost:1337/pages/?slug=',
   siteName: 'Ilton Barbosa',
   defaultSlug: 'landing-page',
